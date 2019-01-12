2ff1b9fb043d8997ddc94ad2dc64e12bcfbc52f6
matthew <matthew.strasiotto@gmail.com>
2019-01-12 13:36:41 +1100

    setup pretty much done

---
+++ R/portrpaths.R
@@ -0,0 +1,7 @@
+
+#' Portable Paths for Network Drive Dependencies
+#'
+#' This tool allows configuration of paths on network drives to be machine
+#' independant, and yaml configurable
+#' @name portrpaths-package
+NULL
