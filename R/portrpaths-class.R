83cf3a2bd4034471564828ddde1cef456d8f03be
matthew <matthew.strasiotto@gmail.com>
2019-01-12 14:14:20 +1100

    deciding how generic to make this
    Will make this applicable to our use-case for now, and give though to extending it

---
+++ R/portrpaths-class.R
@@ -0,0 +1,24 @@
+
+#' Main class for portrpaths
+#'
+#'
+#' R6 Reference class encapsulating all portrpaths behaviour and data
+#'
+#' @name portrpaths-class
+NULL
+
+PortrPaths <-
+R6::R6Class("PortrPaths",
+public = list(
+  # Variables =================================================================
+
+  # Methods ===================================================================
+),
+private = list(
+  # Variables =================================================================
+  nm = NULL,
+  d_root = NULL
+
+
+  # Methods ===================================================================
+))
