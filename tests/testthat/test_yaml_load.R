6025fe2eab78526e9e1a7244f63aeb99bba42890
matthew <matthew.strasiotto@gmail.com>
2019-01-17 13:26:21 +1100

    Testing improved, tests added

--- tests/testthat/test_yaml_load.R
+++ tests/testthat/test_yaml_load.R
@@ -6,10 +6,10 @@ test_that("Can load files in correct directory", {
   local <- file.path(dir, "local.yaml")
   shared <- file.path(dir, "shared.yaml")
 
-  setup_t1_example(local, shared)
+  setup_t1(local, shared)
 
   portrpath <- PortrPath$new(local, shared)
-  f_paths <- portrpath$get_file_paths()
+  f_paths <- portrpath$files
 
   for (nm in names(f_paths)){
     f <- f_paths[[nm]]
@@ -18,4 +18,19 @@ test_that("Can load files in correct directory", {
 
 })
 
+context("Able to find files with abs roots")
+test_that("More complex roots work", {
 
+  dir <- system.file("test_data", "t2", package = "portrpaths")
+  local <- file.path(dir, "local.yaml")
+  shared <- file.path(dir, "shared.yaml")
+  setup_t2(local, shared)
+
+  tp <- PortrPath$new(local, shared)
+  paths <- tp$files
+
+  for (nm in names(paths)){
+    f <- paths[[nm]]
+    expect_true(file.exists(f), info = glue::glue("Can {nm}: {f} be found?"), label = nm)
+  }
+})
