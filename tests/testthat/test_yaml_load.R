5159327bdab162dc51be5a594e48f95233ac4e35
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-28 15:12:22 +1100

    Update test_yaml_load.R

--- tests/testthat/test_yaml_load.R
+++ tests/testthat/test_yaml_load.R
@@ -6,7 +6,11 @@ test_that("Can load files in correct directory", {
 
   setup_t1(local)
 
-  portrpath <- PortrPath$new(local)
+  pp <- PortrPath$new(local)
+
+  root_expected <- setup_t1_expected_root()
+
+  expect_equal(pp$root, root_expected)
 
 })
 
@@ -20,18 +24,40 @@ test_that("More complex roots work", {
 
   tp <- PortrPath$new(local)
 
+  root_expected <- setup_t2_expected_root()
+
+  expect_equal(tp$root, root_expected)
 })
 
 context("Subsequent loads/reloads work correctly")
 
 test_that("Can load/reload first example", {
+  `%||%` <- rlang::`%||%`
+
   dir <- system.file("test_data", "t1", package = "portrpaths")
   local <- file.path(dir, "local.yaml")
 
   setup_t1(local)
+
+  config_preload <- yaml::read_yaml(local)
+
+  expect_null(config_preload$profiles)
+  config_preload$profiles <- config_preload$profiles %||% list()
+  expect_null(config_preload$profiles$default)
+
   p1 <- PortrPath$new(local)
 
+  root_expect <- setup_t1_expected_root()
+  expect_equal(p1$root, root_expect)
+
+  config_loaded <- yaml::read_yaml(local)
+
+  profile_expect <- ".PROJECT_ROOT"
+
+  expect_equal(config_loaded$profiles$default, profile_expect)
 
   p2 <- PortrPath$new(local)
+
+  expect_equal(p1$root, p2$root)
 
 })
